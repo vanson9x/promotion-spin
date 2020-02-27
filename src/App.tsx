0329303b4bf05b14a7b5e6522500adb5960503e2
Phạm Văn Sơn <vanson9x.it@gmail.com>
2020-02-27 23:37:24 +0700

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     const [spin, setSpin] = useState(false);
     return (
         <div className="App">
-            <div className={`container${spin ? ' spinning' : ''}`}>
+            {/*<div className={`container${spin ? ' spinning' : ''}`}>
                 <div className="sec-0">
                     <div className="p-relative">
                         <div className="content">a</div>
@@ -32,18 +32,41 @@ function App() {
                         <div className="content">e</div>
                     </div>
                 </div>
-                <div className="sec-5 plus">
+                <div className="sec-5">
                     <div className="p-relative">
-                        <div className="content"/>
+                        <div className="content">f</div>
                     </div>
                 </div>
             </div>
             <button onClick={() => {
                 spin && setSpin(false);
                 setTimeout(() => setSpin(true));
-                // setTimeout(() => setSpin(false), 2000);
+                setTimeout(() => setSpin(false), 2000);
             }}>Quay
-            </button>
+            </button>*/}
+            <div className="svg">
+                {[0, 1, 2, 3, 4, 5].map((value, index) =>
+                    <svg key={index}
+                         width={600}
+                         height={600}
+                         viewBox={'0 0 600 600'}
+                         style={{transform: `rotate(${(360 / 6) * value}deg)`}}>
+                        <g>
+                            <circle r={150} cx={300} cy={300}
+                                    fill="transparent"
+                                    stroke={index % 2 === 0 ? 'white' : 'orange'}
+                                    strokeWidth={300}
+                                    strokeDasharray={'calc(16.66 * 942 / 100) 942'}>
+                            </circle>
+                        </g>
+                    </svg>)}
+                <div className='content'>
+                    <div>
+                        <p>value 0</p>
+                        <p>value 3</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
